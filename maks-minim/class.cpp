de4921f692146758f24ff62480a45267d6c9110f
ibnuzaman <ibnuzaman2133@gmail.com>
2022-05-18 22:41:24 +0700

    update

--- maks-minim/class.cpp
+++ maks-minim/class.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    int a,maks,min,d;
-    
+class Hitung{
+    public:
+    void input(){
     cout << "Masukkan jumlah data "; cin >> d;
-    for (int i = 1; i <= d ; i++)
+    for (int i = 0; i < d ; i++)
     {
         cout << "Masukkan nilai "; cin >> a;
-        if(d==1){
+        if(i==0){
             min=a;
             maks=a;
         }else{
@@ -20,7 +20,18 @@ int main (){
             }
         }
     }
+
+    }
+   void output(){
     cout << "Nilai maksimal : " << maks << endl;
     cout << "Nilai minimum  : " << min << endl;
-    return 0;
+    
+   }
+   private:
+   int a,maks,min,d;
+};
+int main(){
+    Hitung x;
+    x.input();
+    x.output();
 }
